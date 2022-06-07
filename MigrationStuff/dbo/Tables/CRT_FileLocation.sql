3095d6b36591de690e51aac3e943a9fdd98ec0bf
Conor Donnelly <conor.donnelly@rewardinsight.com>
2022-06-07 10:06:46 +0100

    Delete MigrationStuff directory

--- MigrationStuff/dbo/Tables/CRT_FileLocation.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[CRT_FileLocation] (
-    [MatcherShortName]     VARCHAR (12)  NOT NULL,
-    [VectorID]             INT           NOT NULL,
-    [InboundFileLocation]  VARCHAR (256) NOT NULL,
-    [OutboundFileLocation] VARCHAR (256) NOT NULL,
-    CONSTRAINT [PK_CRT_FileLocation] PRIMARY KEY CLUSTERED ([MatcherShortName] ASC) WITH (FILLFACTOR = 95)
-);
-
