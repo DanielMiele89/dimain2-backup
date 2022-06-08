2acb3b62abc49d31af2a07ad18e76f27c70bb8ef
Conor Donnelly <Conor.Donnelly@rewardinsight.com>
2022-06-08 17:05:21 +0100

    CD_Python_V2

---
+++ Python/Security/RoleMemberships.sql
@@ -0,0 +1,10 @@
+﻿ALTER ROLE [db_ddladmin] ADD MEMBER [Process_Python];
+
+
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [Process_Python];
+
+
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [Process_Python];
+
